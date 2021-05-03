975de126401e3cc4e0ee449ae5d362b039d6ffad
caleb mccombs <cthepyro@gmail.com>
2021-05-03 05:17:58 -0700

    re scoped projectadded pathfinding

---
+++ Assets/scripts/Destination.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Destination : MonoBehaviour
+{
+    [SerializeField] EnemyBehaviour eb;
+    public GameObject pathFrog;
+    public GameObject pathFrogFX;
+    public GameObject patrolFrog;
+    private Transform[] waypoints;
+    private bool onWayBack;
+    private Transform closestWaypoint;
+    private bool home;
+    bool complete;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pathFrog.SetActive(false);
+        waypoints = eb.waypoints;
+        onWayBack = false;
+        complete = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            //switch active frogs
+            //activate path frog
+            pathFrog.SetActive(true);
+            //set transform to patrol frog
+            waypoints = eb.waypoints;
+            pathFrog.transform.position = patrolFrog.transform.position;
+            pathFrogFX.transform.rotation = new Quaternion();
+            pathFrogFX.transform.rotation *= Quaternion.Euler(0, 0, 90);
+            
+            patrolFrog.SetActive(false);
+            var tempMousePosition = Input.mousePosition;
+            tempMousePosition.z = -1;
+            this.transform.position = Camera.main.ScreenToWorldPoint(tempMousePosition);
+            complete = false;
+        }
+        //let path frog rach destination
+        float xDist = pathFrog.transform.position.x - this.transform.position.x;
+        //get to destination
+        float yDist = pathFrog.transform.position.y - this.transform.position.y;
+        if (System.Math.Abs(xDist) < 1 && System.Math.Abs(yDist) < 1 && !onWayBack && !complete)
+        {
+            //pathfrog path finds back to last patrol point
+            closestWaypoint = GetClosestWaypoint(waypoints);
+            this.transform.position = closestWaypoint.position;
+            onWayBack = true;
+        }
+        else if (Vector3.Distance(closestWaypoint.position, this.transform.position) < 1 && onWayBack)
+        {
+            complete = true;
+            onWayBack = false;
+            patrolFrog.SetActive(true);
+            patrolFrog.transform.position = pathFrog.transform.position;
+            patrolFrog.transform.rotation = pathFrog.transform.rotation;
+            for(int i = 0; i < waypoints.Length; i++)
+            {
+                if (closestWaypoint = waypoints[i]) eb.WaypointIndex = i;
+            }
+
+            //change active frogs
+            pathFrog.SetActive(false);
+            
+            //home reached
+
+        }
+
+
+        //change frog rotation because math is hard
+
+    }
+    Transform GetClosestWaypoint(Transform[] waypoints)
+    {
+        Transform tMin = null;
+        float minDist = Mathf.Infinity;
+        Vector3 currentPos = transform.position;
+        for (int i = 0; i< waypoints.Length;i++)
+        {
+            float dist = Vector3.Distance(waypoints[i].position, currentPos);
+            if (dist < minDist)
+            {
+                tMin = waypoints[i];
+                minDist = dist;
+            }
+        }
+        return tMin;
+    }
+}
--- Assets/scripts/EnemyBehaviour.cs
+++ Assets/scripts/EnemyBehaviour.cs
@@ -9,45 +9,23 @@ public class EnemyBehaviour : MonoBehaviour
     public Transform[] waypoints;
     public int alertState;
 
-    private int WaypointIndex;
+    public int WaypointIndex;
     private float dist;
-    public Transform Gun;
-    public Transform player;
-    public playerControls playerControls;
-    public GameObject bulletPrefab;
 
-    public float bulletForce;
-
-    public int rayDist;
-    public int timeBetweenShots;
-    private bool counting;
-    private float targetTime;
+    
     // Start is called before the first frame update
     void Start()
     {
         WaypointIndex = 0;
-        alertState = 0;
-        counting = false;
+        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (alertState == 0)
-        {
-            NormalPatrol();
-        }
-        else if(alertState == 1)
-        {
-            ShootAtPlayer();
-        }
-        //eyes
-        RaycastHit2D hit = Physics2D.Raycast(this.transform.position, transform.right, rayDist);
-        Debug.DrawRay(transform.position, hit.point);
-        if (hit.collider.name == "Player")
-        {
-            alertState = 1;
-        }
+       
+        Patrol();
     }
     void MoveTowards()
     {
@@ -61,7 +39,7 @@ public class EnemyBehaviour : MonoBehaviour
             WaypointIndex = 0;
         }
     }
-    void NormalPatrol()
+    void Patrol()
     {
         float totalDeltaAngle = Vector2.Angle(transform.right, (waypoints[WaypointIndex].position - transform.position).normalized);
 
@@ -77,7 +55,8 @@ public class EnemyBehaviour : MonoBehaviour
         transform.rotation *= Quaternion.AngleAxis(finalDeltaAngle * rotDir, Vector3.back);
 
         float dot = Vector2.Dot(transform.right, (waypoints[WaypointIndex].position - transform.position).normalized);
-        
+        //-.01389242
+        //0.07321681
         if (dot > .99 && dot < 1.01)
         {
             MoveTowards();
@@ -88,57 +67,6 @@ public class EnemyBehaviour : MonoBehaviour
             }
         }
     }
-    void ShootAtPlayer()
-    {
-        //rotate towards player
-        float totalDeltaAngle = Vector2.Angle(transform.right, (player.position - transform.position).normalized);
-
-        Vector2 a = transform.right;
-        Vector2 b = (player.position - transform.position).normalized;
-        Vector2 d = Vector2.Perpendicular(b);
-
-        float dotAD = Vector2.Dot(a, d);
-        float rotDir = dotAD > 0 ? 1 : -1;
-
-        float finalDeltaAngle = Mathf.Min(totalDeltaAngle, Time.deltaTime * rotationSpeed*10);
-
-        transform.rotation *= Quaternion.AngleAxis(finalDeltaAngle * rotDir, Vector3.back);
-
-        float dot = Vector2.Dot(transform.right, (player.position - transform.position).normalized);
-
-        
-        if (!counting)
-        {
-            counting = true;
-            targetTime = Time.time + timeBetweenShots;
-        }
-        else
-        {
-            
-            float currentTime = Time.time;
-            if(currentTime>= targetTime)
-            {
-                counting = false;
-                if(Physics2D.Raycast(this.transform.position, transform.right, rayDist).collider.name == "Player")
-                {
-                    Shoot();
-                }
-                else
-                {
-                    alertState = 0;
-                }
-                
-            }
-            
-        }
-        //every 2seconds
-            //shoot at player
-    }
-    void Shoot() 
-    {
-        Debug.DrawLine(this.transform.position, player.position, Color.red, 0.5f);
-        playerControls.health--;
-        Debug.Log("health: " + playerControls.health);
-    }
+   
 }
 
